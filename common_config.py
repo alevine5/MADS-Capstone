eca6bf1a6fc4147d7dc36fe4495af907d2cd52ed
alevine5 <ajlev@umich.edu>
2023-12-10 19:37:37 -0800

    updating filepaths in all nbs to reflect new project structure, added __init__.py to new packages, added Dash exploration nb

--- common_config.py
+++
@@ -1,6 +0,0 @@
-# common_config.py
-import os
-
-# set a standard directory for each notebook
-default_working_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-os.chdir(default_working_directory)
